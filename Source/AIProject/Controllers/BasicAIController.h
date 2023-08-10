5862cc311ad2a43fc39722f9a33de266ca7e36dc
textfox233 <78984659+textfox233@users.noreply.github.com>
2023-08-10 15:09:55 +0100

    New AI Controller
    Added an empty AI controller class and created a blueprint child of it.

---
+++ Source/AIProject/Controllers/BasicAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "BasicAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIPROJECT_API ABasicAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
