a19db85cb275e0e166fbda22d822d46ede80dd1d
textfox233 <78984659+textfox233@users.noreply.github.com>
2023-08-17 13:08:08 +0100

    Spot & Follow
    AI uses the perception component to spot the player and will follow new behaviors when it has done so
    
    Need to implement enum states to the AI Controller to follow further

--- Source/AIProject/Controllers/BasicAIController.cpp
+++ Source/AIProject/Controllers/BasicAIController.cpp
@@ -36,50 +36,51 @@ void ABasicAIController::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
-	// get the target player from the blackboard
-	UObject* TargetPlayer = GetBlackboardComponent()->GetValueAsObject(TEXT("TargetPlayer"));
+	//// get the target player from the blackboard
+	//UObject* TargetPlayer = GetBlackboardComponent()->GetValueAsObject(TEXT("TargetPlayer"));
 
-	// if it's not set find a new target
-	if (!TargetPlayer)
-	{
-		TargetPlayer = ChooseTarget();
+	//// if it's not set find a new target
+	//if (!TargetPlayer)
+	//{
+	//	TargetPlayer = ChooseTarget();
 
-		// set blackboard value						// key					// value
-		GetBlackboardComponent()->SetValueAsObject	(TEXT("TargetPlayer"),	TargetPlayer);
-	}
+	//	// set blackboard value						// key					// value
+	//	GetBlackboardComponent()->SetValueAsObject	(TEXT("TargetPlayer"),	TargetPlayer);
+	//}
 
-	// if you have a target player
-	if (TargetPlayer != nullptr)
-	{
-		// cast it
-		if (APawn* PlayerPawn = Cast<APawn>(TargetPlayer))
-		{
-			// check they're within line of sight
-			if (LineOfSightTo(PlayerPawn))
-			{
-				// save their position on blackboard		// key								// value
-				GetBlackboardComponent()->SetValueAsVector	(TEXT("PlayerLocation"),			PlayerPawn->GetActorLocation());
-				GetBlackboardComponent()->SetValueAsVector	(TEXT("LastKnownPlayerLocation"),	PlayerPawn->GetActorLocation());
+	//// if you have a target player
+	//if (TargetPlayer != nullptr)
+	//{
+	//	// cast it
+	//	if (APawn* PlayerPawn = Cast<APawn>(TargetPlayer))
+	//	{
+	//		// check they're within line of sight
+	//		if (LineOfSightTo(PlayerPawn))
+	//		{
+	//			// save their position on blackboard		// key								// value
+	//			GetBlackboardComponent()->SetValueAsVector	(TEXT("PlayerLocation"),			PlayerPawn->GetActorLocation());
+	//			GetBlackboardComponent()->SetValueAsVector	(TEXT("LastKnownPlayerLocation"),	PlayerPawn->GetActorLocation());
 
-			}
-			// otherwise clear the player location
-			else
-			{
-				/// debug msg
-				//if (GEngine)
-				//{
-				//	GEngine->AddOnScreenDebugMessage(
-				//		-1,
-				//		15.f,
-				//		FColor::Yellow,
-				//		FString(TEXT("Player not visible"))
-				//	);
-				//}
-				//GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), );
-				GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
-			}
-		}
-	}
+	//		}
+	//		// otherwise clear the player location
+	//		else
+	//		{
+	//			/// debug msg
+	//			//if (GEngine)
+	//			//{
+	//			//	GEngine->AddOnScreenDebugMessage(
+	//			//		-1,
+	//			//		15.f,
+	//			//		FColor::Yellow,
+	//			//		FString(TEXT("Player not visible"))
+	//			//	);
+	//			//}
+
+	//			//GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), );
+	//			GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+	//		}
+	//	}
+	//}
 }
 
 APawn* ABasicAIController::ChooseTarget()
